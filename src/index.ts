453623fb5209c31d7d4553ff0ccb07f689f8089e
Aaron Strick <aaronstrick@gmail.com>
2024-06-10 23:40:25 -0700

    move songs to data file

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,11 @@
 import { Elm } from "./Main.elm";
+import { SONGS } from "./songs"
+
 
 const ELEMENT_ID = "golem-mount-point"
 
 const ARTIST = "Sam Reider & The Human Hands"
 const ALBUM = "The Golem and Other Tales"
-const SONGS = [
-  {
-    name: "In Darkness, a Rabbi's Prayer",
-    audioUrl: "static/songs/1.mp3",
-    imageUrl: "static/images/1.jpg",
-  },
-  {
-    name: "A Mysterious Stranger With An Extraordinary Idea",
-    audioUrl: "static/songs/2.mp3",
-    imageUrl: "static/images/2.jpg"
-  },
-  {
-    name: "Gathering Clay, Making a Man",
-    audioUrl: "static/songs/3.mp3",
-    imageUrl: "static/images/3.jpg"
-  },
-  {
-    name: "Awakening Life With a Word",
-    audioUrl: "static/songs/4.mp3",
-    imageUrl: "static/images/4.jpg"
-  },
-  {
-    name: "A Power Too Great To Control",
-    audioUrl: "static/songs/5.mp3",
-    imageUrl: "static/images/5.jpg"
-  },
-  {
-    name: "The Golem Falls In Love",
-    audioUrl: "static/songs/6.mp3",
-    imageUrl: "static/images/6.jpg"
-  },
-  {
-    name: "The Rabbi Chases and Destroys His Creation",
-    audioUrl: "static/songs/7.mp3",
-    imageUrl: "static/images/7.jpg"
-  },
-  {
-    name: "Return To Mud",
-    audioUrl: "static/songs/8.mp3",
-    imageUrl: "static/images/8.jpg"
-  },
-]
 
 const node = document.getElementById(ELEMENT_ID);
 
