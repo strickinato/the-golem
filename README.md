677ac3f5e37924bc39aebed4a48320c70c402238
Aaron Strick <aaronstrick@gmail.com>
2024-06-11 00:23:12 -0700

    notes about sam's progress

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Golem
+
+
+
+
+
+### Quickly generating the code to copy into squarespace:
+
+`rm -rf dist && npm run build && cat dist/index.*.js | pbcopy`
+
+
+
+### 06/20/24 TODO Media:
+  - Convert the play button and stuff from webp to jpg
+  - Trim the main images so there isn't extra black space
+  - Get a new version of #4 because it looks fucked up
+  - Re-upload them and add links
+  - Make the cutouts of the roman numberals
+  - Upload all the songs and get the urls
+
+
+## Random note about the blue:
+
+rgb - 61, 135, 212
---
+++ src/songs-squarespace.js
@@ -0,0 +1,12 @@
+export const SONGS = [
+  {
+    name: "In Darkness, a Rabbi's Prayer",
+    audioUrl: "https://static1.squarespace.com/static/5457d939e4b0d5b75c105e2f/t/6667dd89490aca5d53b5ab95/1718082960021/1.mp3/original/1.mp3",
+    imageUrl: "https://images.squarespace-cdn.com/content/v1/5457d939e4b0d5b75c105e2f/15cfd1e3-85f7-4788-8f5d-b0f521b4808f/1.jpg?format=2500w",
+  },
+  {
+    name: "A Mysterious Stranger With An Extraordinary Idea",
+    audioUrl: "https://static1.squarespace.com/static/5457d939e4b0d5b75c105e2f/t/6667f1859338655ff42489c0/1718088074006/2.mp3/original/2.mp3",
+    imageUrl: "https://images.squarespace-cdn.com/content/v1/5457d939e4b0d5b75c105e2f/1957dae0-06c2-4751-9e29-1b8622cb936e/2.jpg?format=2500w"
+  },
+]
